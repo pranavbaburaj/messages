dd2b2a1883323fe98b5ebf659ade91226f2e4135
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-03-17 12:08:52 +0530

    Formatted the newly created mesages/static/js/csrf.js

--- messages/static/js/csrf.js
+++ messages/static/js/csrf.js
@@ -1,16 +1,16 @@
 
-export function getCookieElement(key){
+export function getCookieElement(key) {
     const splitText = document.cookie.toString().split(";")
-    console.log( splitText )
-    for(let cidx=0; cidx<splitText.length; cidx++){
+    console.log(splitText)
+    for (let cidx = 0; cidx < splitText.length; cidx++) {
         const currentKey = splitText[cidx]
         console.log(currentKey)
-        for(let eidx=0; eidx<currentKey.length; eidx++){
-            if(currentKey[eidx] == "="){
-                if(currentKey.slice(0, eidx) == key){
-                    return currentKey.slice(eidx+1, currentKey.length)
+        for (let eidx = 0; eidx < currentKey.length; eidx++) {
+            if (currentKey[eidx] == "=") {
+                if (currentKey.slice(0, eidx) == key) {
+                    return currentKey.slice(eidx + 1, currentKey.length)
                 }
             }
         }
-    } 
+    }
 }
